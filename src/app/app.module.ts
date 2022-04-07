b31779cf57d223a3b064058984ec456a8c1f052b
Gabriel1999ap <gabriel.ap.almeida@outlook.com>
2022-04-06 22:23:24 -0300

    extract table creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { novaTransferenciaComponent } from './novaTransferencia/novaTransferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   declarations: [
     AppComponent,
-    novaTransferenciaComponent
+    novaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt' },
+  {
+    provide:DEFAULT_CURRENCY_CODE,
+useValue:'BRL'
+  },  
+],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/novaTransferencia/novaTransferencia.component.ts
+++ src/app/novaTransferencia/novaTransferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+
 
 @Component({
   selector: 'app-novaTransferencia',
@@ -8,13 +9,23 @@ import { Component } from "@angular/core";
 
 export class novaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter <any>();
+
+
   valor:number;
   destino:number;
 
   transferir() {
     console.log("Solicitada nova transferencia")
-    console.log("valor: ",this.valor);
-    console.log("destino: ",this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir)
+    
+    this.limparCampos()
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 
 }
